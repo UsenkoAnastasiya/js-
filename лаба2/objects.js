004d3e26b5ab9137dd580361617eac5291af6793
UsenkoAnastasiya <usenkonasty@gmail.com>
2025-10-20 23:11:06 +0300

    Create objects.js

---
+++ лаба2/objects.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const fn = (x) =>{
+    const obj = { name : "Andry"};
+    let vobj={ name : "Mary"};
+    obj.name="AAndry";
+    vobj.name="MMary";
+   vobj={name : "CangeMary"} 
+};
+
+
+module.exports = { fn };
