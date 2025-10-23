cbbf84b5be27cf968be764fe9c478af41b933476
UsenkoAnastasiya <usenkonasty@gmail.com>
2025-10-23 14:41:49 +0300

    Add generateKey function to create random keys

---
+++ лаба3/2-key.js
@@ -0,0 +1,10 @@
+'use strict';
+const characters = "absdfjkdvoeifnsklckdk";
+const generateKey = (length, possible) => {
+  // Generate string of random characters
+  // Use Math.random() and Math.floor()
+  // See documentation at MD
+  return key;
+};
+
+module.exports = { generateKey };
