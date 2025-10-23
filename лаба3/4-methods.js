bece0146dd0588db13a826a15197bedc36f830be
UsenkoAnastasiya <usenkonasty@gmail.com>
2025-10-24 00:07:59 +0300

    Update 4-methods.js

--- лаба3/4-methods.js
+++ лаба3/4-methods.js
@@ -1,1 +1,22 @@
+'use strict';
+const iface ={
+    m1: x => [x],
+  m2: function (x, y) {
+    return [x, y];
+  },
+  m3(x, y, z) {
+    return [x, y, z];
+  },
+} 
+const methods = iface => {
+  const names = [ ];
+  for(const key in iface){
+    const value = iface[key] ;
+      if (typeof value === 'function') {
+    names.push([key, value.length]);
+  }
 
+  }
+};
+
+module.exports = { methods };
