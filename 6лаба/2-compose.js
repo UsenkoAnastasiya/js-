f6404fb65ce14b0eeb6f0c47bf84ad566a1151ab
UsenkoAnastasiya <usenkonasty@gmail.com>
2025-11-11 17:28:39 +0200

    Update 2-compose.js

--- 6лаба/2-compose.js
+++ 6лаба/2-compose.js
@@ -1,1 +1,19 @@
+'use strict';
 
+const compose = (...fns) => {
+const handlers=[]; 
+    const composed = (x) =>{
+ try{  
+    return fns.reduceRight((v, f) => f(v), x);
+} catch(error){
+   for (const handler of handlers) {
+        handler(error); 
+      }
+}
+};
+composed.on=(name,handler)=>
+ { if(name==='error') handlers.push(handler);
+}
+ return composed;
+};
+module.exports = { compose };
